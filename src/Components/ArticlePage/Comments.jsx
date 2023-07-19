1f10293e9b3f10f31cead7098bd746dc7ada65a8
anisingh45 <98647034+anisingh45@users.noreply.github.com>
2023-07-19 09:06:47 +0530

    asaA

---
+++ src/Components/ArticlePage/Comments.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Comments() {
+    return (
+        <>
+            <div className='' >
+                <div className="flex ">
+                    <div className="py-5 bg-[#f7f6f4] w-full">
+                        <span className="text-[#2c3a4a]   font-sans font-[700] text-[1rem] leading-[1.5rem] bg-yellow ">
+                            Tanguy Bosser, Souhaib Ben Taieb
+                        </span>
+                    </div>
+                </div>
+            </div>
+            
+        </>
+    )
+}
+
+export default Comments
